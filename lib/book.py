665de6cb7d37ab01193bde6cbf9eb67875ac3aa0
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-24 03:07:15 -0600

    to set properties and enforce use of getters and setters use name=property(getter, setter) and inside those methods use _name to avoid infinite recursion

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, numpgs):
+        self.title = title;
+        self.set_page_count(numpgs);
+
+    def get_page_count(self): return self._page_count;
+
+    def set_page_count(self, val):
+        #print(f"val = {val}");
+        if (type(val) == int):
+            print("in setter before changing the value!");
+            self._page_count = val;
+        else: print("page_count must be an integer");
     
-        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!");
+
+    page_count = property(get_page_count, set_page_count);
+
+#bk = Book("name", 10);
+#bk.page_count = "str";
+#print(f"bk.page_count = {bk.page_count}");
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, name, size):
+        self.brand = name;
+        self.setSize(size);
+        self.conditon = "old";
+    
+    def setSize(self, val):
+        if (type(val) == int): self._size = val;
+        else: print("size must be an integer");
+    
+    def getSize(self): return self._size;
+
+    size = property(getSize, setSize);
+
+    def cobble(self):
+        self.condition = "New";
+        print("Your shoe is as good as new!");
